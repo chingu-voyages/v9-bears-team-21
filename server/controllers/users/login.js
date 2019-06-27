3d1061b4890c43d7b4ca0e5be56ec2543cf38924
sulenchy <sulaimanabiodun172@gmail.com>
2019-06-27 12:40:11 +0100

    refactor the login implementation

--- server/controllers/users/login.js
+++ server/controllers/users/login.js
@@ -1,5 +1,5 @@
 import bcrypt from 'bcryptjs';
-import db from '../../server/models/index'
+import db from '../../models'
 
 let { Users } = db;
 
@@ -11,15 +11,17 @@ login.get = (req, res) => {
 
 login.post = (req, res) => {
   const { username, password } = req.body;
-  Users.findAll({
+  Users.findOne({
     attributes: ['password', 'user_uid', 'role'],
     where: {
         username
     }
   })
   .then(user => {
-    if (!user || !user.length) return res.send({err: "No User Found"})
-    const userData = user[0].dataValues;
+    if (!user) {
+      return res.status(404).json({err: "No User Found"})
+    }
+    const userData = user.dataValues;
     bcrypt.compare(password, userData.password)
     .then(match => {
       if (match) {
@@ -27,13 +29,19 @@ login.post = (req, res) => {
           user_uid: userData.user_uid,
           role: userData.role,
         }
-        req.session = user
+        req.session = user;
+        res.status(200).json({
+          message: "User login successfully"
+        })
       }
-      res.send({match})
+      return res.status(404).json({
+        err: "Password is incorrect. Please try again"
+      })
     })
-    .catch(err => console.log(err))
   })
-  .catch(err => console.log(err))
+  .catch(err => res.status(500).json({
+      err
+  }))
 };
 
 export { login }
--- tests/server/app.test.js
+++ tests/server/app.test.js
@@ -1,14 +1,10 @@
 import chai from 'chai';
-import bcrypt from 'bcryptjs';
 import chaiHttp from 'chai-http';
 import server from '../../server/app';
-import db from '../../server/models/index'
 
 chai.use(chaiHttp);
 
-const { expect, should, assert } = chai;
-
-let { Users } = db;
+const { expect } = chai;
 
 describe('App.js', () => {
   it('default api route', (done) => {
@@ -20,80 +16,5 @@ describe('App.js', () => {
         expect(res.text).to.equal('Welcome to EduGate!');
         done();
       });
-  })
-})
-
-describe('Login', () => {
-
-  before((done) => {
-    bcrypt.hash('123testing', 10)
-    .then(password => {
-      Users.create({
-            user_uid: '40e6215d-b5c6-4896-987c-f30f3678f608',
-            first_name: 'John',
-            last_name: 'Doe',
-            dob: new Date(),
-            year_of_graduation: '2020',
-            role: 'admin',
-            password,
-            phone_number: '07038015455',
-            username: 'jamsgra',
-            email: 'John.doe@gmail.com',
-        })
-        .then(() => done())
-    })
-  })
-
-  after((done) => {
-    Users.destroy({
-      where: {
-        username: 'jamsgra'
-      }
-    }).then(() => done())
-  })
-
-  it('login success', (done) => {
-    chai.request(server)
-      .post('/login')
-      .type('form')
-      .send({
-        'username': 'jamsgra',
-        'password': '123testing'
-      })
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        expect(res.body.match).to.equal(true);
-        done();
-      })
-  })
-
-  it('login password fail', (done) => {
-    chai.request(server)
-      .post('/login')
-      .type('form')
-      .send({
-        'username': 'jamsgra',
-        'password': 'wrongpassword'
-      })
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        expect(res.body.match).to.equal(false);
-        done();
-      })
-  })
-
-  it('login user fail', (done) => {
-    chai.request(server)
-      .post('/login')
-      .type('form')
-      .send({
-        'username': 'wronguser',
-        'password': 'wrongpassword'
-      })
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        expect(res.body.err).to.equal('No User Found');
-        done();
-      })
   })
 })
